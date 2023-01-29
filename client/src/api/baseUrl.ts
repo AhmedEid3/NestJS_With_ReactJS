e8073e79d3323f7d3176e8503ca7ed44c6e0145a
AhmedEid3 <ahmed.eid3@outlook.com>
2023-01-29 13:29:14 +0200

    Update baseUrl for production env

--- client/src/api/baseUrl.ts
+++ client/src/api/baseUrl.ts
@@ -1,1 +1,4 @@
-export const baseUrl = 'http://127.0.0.1:3000/api/';
+const isProd = process.env.NODE_ENV === 'production';
+
+export const baseUrl =
+  (isProd ? 'http://139.59.16.64:3000/' : 'http://127.0.0.1:3000/') + 'api/';
